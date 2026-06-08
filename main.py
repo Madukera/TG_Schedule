a858a97126245cd153e8331d5ee4f7cc22b5cef8
Madukera <valeramoroz2114@gmail.com>
2026-06-08 18:06:10 +0200

    added some functions and main function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import telebot
 import json
+from datetime import datetime, timedelta
+import time
 
 # Import and store your API-token from toke.txt file
-Token = open("token.txt", "r")
-storeToken = Token.read()
-bot = telebot.TeleBot(storeToken)
+with open("token.txt", "r") as file:
+    storeToken = file.read()
+    bot = telebot.TeleBot(storeToken)
 
 '''
 @bot.message_handler(content_types=['help', 'start'])
@@ -16,14 +18,48 @@ def send_welcome(message):
     else:
         bot.send_message(message.from_user.id, "I don't understand you")
 '''
+# Welcome message for /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hi, you just launched the cleaning bot!")
 
 @bot.message_handler(content_types=['text'])
 def echo_message(message):
     user_id = message.from_user.id
-    messsage_text = message.text
-    if messsage_text.lower() == "hello":
+    message_text = message.text
+    if message_text.lower() == "hello":
         bot.reply_to(message, str(user_id))
     else:
         bot.send_message(user_id, message.text)
+
+def load_data_json():
+    with open("users.json", "r") as date:
+        data = json.load(date)
+        return data
+
+
+def get_current_monday():
+    today = datetime.today()
+    monday_convert = today - timedelta(days = today.weekday())
+    monday = monday_convert.strftime("%Y-%m-%d")
+    return monday
+
+def get_next_monday():
+    current = get_current_monday()
+    next_monday_date = datetime.strptime(current, "%Y-%m-%d")
+    next_monday = next_monday_date + timedelta(days = 7)
+    return next_monday.strftime("%Y-%m-%d")
+
+def check_schedule():
+    with open("users.json", "r") as date:
+        date = json.load(date)
+
+
+
+x = get_current_monday()
+y = get_next_monday()
+print(y)
+
+
 
 bot.infinity_polling()
